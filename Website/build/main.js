c1d00bfe1658e572a55e4aee81e1ab57f928b5fb
Gavin Koehler <gkoehler@fixationco.com>
2014-06-28 19:01:47 -0500

    Header

--- Website/build/main.js
+++ Website/build/main.js
@@ -11304,3 +11304,17 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
 }(jQuery);
 ;/* some source code for the javascript of the web site internet */
 
+$('[data-scrollto]').click(function(){
+	var scrollTo = $(this).attr('data-scrollto');
+	$('html, body').animate({
+        scrollTop: $(scrollTo).offset().top
+    }, 500);
+});
+
+$("a[href^='#']").click(function() {
+	var scrollTo = $(this).attr('href');
+	$('html, body').animate({
+        scrollTop: $(scrollTo).offset().top
+    }, 500);
+    return false;
+});
--- Website/src/g-and-m.js
+++ Website/src/g-and-m.js
@@ -1,2 +1,16 @@
 /* some source code for the javascript of the web site internet */
 
+$('[data-scrollto]').click(function(){
+	var scrollTo = $(this).attr('data-scrollto');
+	$('html, body').animate({
+        scrollTop: $(scrollTo).offset().top
+    }, 500);
+});
+
+$("a[href^='#']").click(function() {
+	var scrollTo = $(this).attr('href');
+	$('html, body').animate({
+        scrollTop: $(scrollTo).offset().top
+    }, 500);
+    return false;
+});
